b53f32c1426a4189a6d2ac9a6a62c56c911dedbf
Charles Jacquin <charles.jacquin@autistici.org>
2015-06-22 22:39:33 +0200

    start

--- app/src/users/UserController.spec.js
+++ app/src/users/UserController.spec.js
@@ -1,24 +1,15 @@
 describe('UserController', function(){
 
     var UserController;
-    var user = {
-        name: 'foo'
-    };
 
     beforeEach(module('users'));
 
     beforeEach(inject(function($controller){
         UserController = $controller('UserController');
-        UserController.selectedIndex = 0;
-        UserController.users[0] = user;
     }));
 
     it('should have an array of user defined', function(){
         expect(UserController.users.length).toBe(6);
     });
 
-    it('should give selectedUser attribute a value when userChange method is called', function(){
-        UserController.userChange();
-        expect(UserController.selectedUser).toBe(user);
-    });
 });
