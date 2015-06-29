05daf6636747115dc6bcc8670ce9eb2a516dd1f7
Mikael Couzic <mikaelcouzic@gmail.com>
2015-06-29 09:29:37 +0200

    commentModel

--- app/src/model/commentModel.js
+++ app/src/model/commentModel.js
@@ -2,12 +2,21 @@
 
     angular.module('model')
 
-        .service('commentModel', function () {
+        .service('commentModel', function ($http) {
 
+            var URI = 'http://localhost:3000/comments';
             var commentModel = this;
 
-            commentModel.create = function () {
+            commentModel.create = function (comment) {
+                return $http.post(URI, comment).then(function (response) {
+                    return response.data;
+                });
+            };
 
+            commentModel.findByMovie = function (movieId) {
+                return $http.post(URI + '/movie?' + movieId).then(function (response) {
+                    return response.data;
+                });
             }
 
         });
